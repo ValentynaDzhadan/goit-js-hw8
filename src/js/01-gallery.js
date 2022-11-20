b1c81331b1e77b9e65e1cb11d0d57c05c55d9c06
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2022-11-21 01:00:28 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryBoxElem: document.querySelector('.gallery'),
+};
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(elem => {
+      return `<a class="gallery__item" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+</a>`;
+    })
+    .join('');
+  return markup;
+}
+
+refs.galleryBoxElem.innerHTML = createMarkup(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
